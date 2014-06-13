dc14ca89ac3d6173dd5d1c3dab52f9f7699b009e
nikisha1992 <nikishashah25@gmail.com>
2014-06-13 19:35:33 +0530

    changes

---
+++ ADInternalNew/Models/ClientModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ADInternalNew.Core;
+using ADInternalNew.DAL;
+
+namespace ADInternalNew.Models
+{
+    public class ClientModel
+    {
+        AdmisInternalDataContext context;
+        public ClientModel(AdmisInternalDataContext pcontext = null)
+		{
+			if (context == null)
+			{
+				context = new AdmisInternalDataContext();
+			}
+			else
+			{
+				context = pcontext;
+			}
+		}
+        public List<User> GetClients(string term)
+        {
+            //var subquery = (from role in context.Roles where role.Role_Name=="Client" select role.Role_Id).Single();
+            var result = (from client in context.Users
+                          join role in context.User_To_Role_Mappings on client.User_Id equals role.User_Id
+                          where (client.First_Name.ToLower().Contains(term.ToLower()) || client.Last_Name.ToLower().Contains(term.ToLower())) && role.Role_Id == Convert.ToInt32(Enums.Roles.Client)
+                          select client).ToList();
+            return result.ToList<User>();
+        }
+    }
+}
